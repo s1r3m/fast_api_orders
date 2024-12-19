db2b59c8c82285007fc73053b87f4b9c610c5d36
s1r3m <falexandrov@gmail.com>
2024-12-19 21:59:32 +0200

    DAY-1 add ws

--- trader/main.py
+++ trader/main.py
@@ -1,16 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
-from middleware import DelayMiddleware, ValidationErrorMiddleware
+from middleware import DelayMiddleware
 from routes import router
+from web_socket import ws_router
 
 app = FastAPI()
 
-# Add custom middleware
-app.add_middleware(ValidationErrorMiddleware)
+# Add custom middleware.
 app.add_middleware(DelayMiddleware)
 
-# Register the router
+# Register the routers.
 app.include_router(router)
+app.include_router(ws_router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True)
