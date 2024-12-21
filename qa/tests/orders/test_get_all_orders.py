5167a18978d2797a518611b663eded2d90772638
s1r3m <falexandrov@gmail.com>
2024-12-21 20:32:46 +0200

    DAY-3 attempt to report allure

--- qa/tests/orders/test_get_all_orders.py
+++ qa/tests/orders/test_get_all_orders.py
@@ -9,7 +9,7 @@ def test_get_all_orders__no_orders__empty_response(actions):
     actions.assertion.check_all_orders_response(response)
 
 
-def test_get_all_orders__mixed_orders__orders_in_response(actions, pending_order, cancelled_order):
+def test_get_all_orders__mixed_orders__orders_in_response(actions, cancelled_order, pending_order):
     response = actions.user.get_all_orders()
     actions.assertion.check_all_orders_response(response, pending_order, cancelled_order)
 
