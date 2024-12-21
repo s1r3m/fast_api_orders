8c5cbe262b2a28bee299fae41d17013cdf26f07a
Filipp Aleksandrov <falexandrov@gmail.com>
2024-12-21 10:09:14 +0200

    DAY-3 run tests in docker (#5)

--- qa/docker/app/start.sh
+++ qa/docker/app/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-# Run migrations
+# Run migrations.
 sed -i 's/localhost:5432/db/' /migrations/alembic.ini
-sleep 3
+sleep 3  # Wait for DB to init
 cd / && poetry run alembic -c /migrations/alembic.ini upgrade head
 
 nginx
-poetry run uvicorn main:app --host 0.0.0.0 --port 8000
+poetry run uvicorn main:app --workers 2 --host 0.0.0.0 --port 8000
