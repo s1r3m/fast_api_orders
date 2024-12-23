ee242d04a104486008e2df5d63e2156299aa0b72
s1r3m <falexandrov@gmail.com>
2024-12-23 11:28:36 +0200

    DAY-5 fix index.html

--- qa/scripts/generate_index.py
+++ qa/scripts/generate_index.py
@@ -1,20 +1,21 @@
 from pathlib import Path
 
-REPORT_DIR = Path().absolute().parent / 'reports'
-INDEX_FILE = REPORT_DIR.parent / ('index.html')
+PROJECT_ROOT = Path().absolute().parent
+REPORT_DIR = PROJECT_ROOT / 'reports'
+INDEX_FILE = PROJECT_ROOT / 'index.html'
 
 
 def main() -> None:
     if not REPORT_DIR.exists():
-        print(f'{REPORT_DIR.absolute()} directory does not exist.')
+        print(f'{REPORT_DIR} directory does not exist. {PROJECT_ROOT=}')
         return
 
-    reports = [file_obj for file_obj in REPORT_DIR.iterdir() if file_obj.is_dir()]
+    reports = [file_obj.parts[-1] for file_obj in REPORT_DIR.iterdir() if file_obj.is_dir()]
 
     with INDEX_FILE.open('w') as f:
         f.write('<html><body><h1>Allure Reports</h1><ul>')
         for report in reports:
-            f.write(f'<li><a href="{report}/index.html">{report}</a></li>')
+            f.write(f'<li><a href="reports/{report}/index.html">{report}</a></li>')
         f.write('</ul></body></html>')
     print(f'Index generated: {INDEX_FILE}')
 
